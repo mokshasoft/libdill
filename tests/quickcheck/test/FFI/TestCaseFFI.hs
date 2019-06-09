4a48fb3b19d8671a91b990ef07d7cc01eb4362eb
Jonas Claeson <jonas.cl@protonmail.com>
2019-06-09 11:43:30 +0200

    The callback compiles but how to make it work?

--- tests/quickcheck/test/FFI/TestCaseFFI.hs
+++ tests/quickcheck/test/FFI/TestCaseFFI.hs
@@ -12,9 +12,11 @@
  -}
 module FFI.TestCaseFFI
   ( ffi_go_sender
+  , ffi_go_sender2
   ) where
 
 import Foreign.C
+import Foreign.Ptr
 
 ffi_go_sender :: CInt -> CInt -> IO (Maybe CInt)
 ffi_go_sender ch val = do
@@ -25,3 +27,18 @@ ffi_go_sender ch val = do
 
 foreign import ccall "ffi_go_sender" internal_ffi_go_sender
   :: CInt -> CInt -> IO CInt
+
+type Callback = IO ()
+
+foreign import ccall "go_coroutine" goCoroutine :: FunPtr Callback -> IO ()
+
+foreign import ccall "wrapper" mkCallback :: Callback -> IO (FunPtr Callback)
+
+ffi_go_sender2 :: CInt -> CInt -> IO (Maybe CInt)
+ffi_go_sender2 ch val = do
+  cbPtr <- mkCallback $ do
+    -- callback code
+    return ()
+  goCoroutine cbPtr
+  freeHaskellFunPtr cbPtr
+  return $ Just val
