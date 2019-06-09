015664773556d33c3bff22b22b351c97748726fa
Jonas Claeson <jonas.cl@protonmail.com>
2019-06-09 12:41:36 +0200

    Add log prints to debug failure of ffi_go_sender2

--- tests/quickcheck/test/FFI/Channels.hs
+++ tests/quickcheck/test/FFI/Channels.hs
@@ -46,6 +46,7 @@ foreign import ccall "dill_chmake" internal_dill_chmake :: Ptr CInt -> IO CInt
 
 dill_chsend_int :: CInt -> CInt -> IO CInt
 dill_chsend_int ch value = do
+  putStrLn $ "dill_chsend_int: channel= " ++ show ch ++ " val= " ++ show value
   val <- malloc
   pokeElemOff val 0 value
   let valSize = fromIntegral (sizeOf value)
